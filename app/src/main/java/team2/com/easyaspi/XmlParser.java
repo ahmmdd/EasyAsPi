c22c5314f8aac9a5a0a6199cf1b5b021ea2fce18
taerakwon <taerakwon@gmail.com>
2017-03-12 12:01:39 -0400

    XML Parser initiated

---
+++ app/src/main/java/team2/com/easyaspi/XmlParser.java
@@ -0,0 +1,26 @@
+package team2.com.easyaspi;
+
+/*
+*   Name: XmlParser.java
+*   Description: XML Parser Class
+*   Last Modified: 2017, March 12
+*   Last Modified By: Taera Kwon
+ */
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class XmlParser {
+    public static void main (String args[]) throws XmlPullParserException, IOException{
+        // Create new instance of the XML Pull Parser in XMPULL V1 API
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        // Specifies that the parser produced by factory will be validating
+        factory.setNamespaceAware(true);
+        // Creating new instance of a XML Pull Parser using the currently configured factory features.
+        XmlPullParser xpp = factory.newPullParser();
+    }
+}
