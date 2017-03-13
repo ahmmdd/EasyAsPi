99c8c9e297faf61c9813d272bf8fc47fbb2bf4d9
taerakwon <taerakwon@gmail.com>
2017-03-12 23:35:45 -0400

    Created Lesson Bean

---
+++ app/src/main/java/team2/com/easyaspi/databasePackage/LessonBean.java
@@ -0,0 +1,65 @@
+package team2.com.easyaspi.databasePackage;
+
+import java.io.Serializable;
+
+/*
+*   Name: LessonBean.java
+*   Description: Lesson Bean Class
+*   Last Modified: 2017, March 12
+*   Last Modified By: Taera Kwon
+ */
+
+public class LessonBean implements Serializable {
+    // PRIVATE VARIABLES
+    private int id, chapter;
+    private String chaptername, topicname, imageaddress, instruction;
+
+    // PUBLIC GETTERS
+    public int getId(){
+        return id;
+    }
+
+    public int getChapter(){
+        return chapter;
+    }
+
+    public String getChaptername(){
+        return chaptername;
+    }
+
+    public String getTopicname(){
+        return topicname;
+    }
+
+    public String getImageaddress(){
+        return imageaddress;
+    }
+
+    public String getInstruction(){
+        return instruction;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setChapter(int chapter){
+        this.chapter = chapter;
+    }
+
+    public void setChaptername(String chaptername){
+        this.chaptername = chaptername;
+    }
+
+    public void setTopicname(String topicname){
+        this.topicname = topicname;
+    }
+
+    public void setImageaddress(String imageaddress){
+        this.imageaddress = imageaddress;
+    }
+
+    public void setInstruction(String instruction){
+        this.instruction = instruction;
+    }
+}
