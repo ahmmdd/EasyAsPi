412a06a259b7e03b371152e0d6f359d6fbcec9ce
domlau <dominic.k.y.lau@gmail.com>
2017-03-19 23:25:56 -0400

    removed unecessary codes

--- app/src/main/java/team2/com/easyaspi/db/DatabaseHelper.java
+++ app/src/main/java/team2/com/easyaspi/db/DatabaseHelper.java
@@ -20,29 +20,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String PROFILE_TABLE_NAME = "profile";
     public static final String PROFILE_ID = "id";
     public static final String PROFILE_NAME = "name";
-    //Database table practice
-    public static final String PRACTICE_TABLE_NAME = "practice";
-    public static final String PRACTICE_PRACTICE_ID = "practiceId";
-    public static final String PRACTICE_QUESTION = "question";
-    //Database table question
-    public static final String LESSON_TABLE_NAME = "lesson";
-    public static final String LESSON_LESSON_ID = "lessonId";
-    public static final String LESSON_LESSON = "lesson;";
+
 
     //Create Table Statement - Profile
     private static final String CREATE_TABLE_PROFILE = "CREATE TABLE " + PROFILE_TABLE_NAME + "(" +
             PROFILE_ID + " INTEGER PRIMARY KEY," + PROFILE_NAME + " TEXT" + ");";
 
-    //Create Table Statement - Lesson
-    private static final String CREATE_TABLE_LESSON = "CREATE TABLE " + LESSON_TABLE_NAME + "(" +
-            LESSON_LESSON_ID + " INTEGER PRIMARY KEY," + LESSON_LESSON + " BLOB" + ")";
-
-    //Create Table Statement - Practice
-    private static final String CREATE_TABLE_PRACTICE = "CREATE TABLE " + PRACTICE_TABLE_NAME + "(" +
-            PRACTICE_PRACTICE_ID + " INTEGER PRIMARY KEY," + PRACTICE_QUESTION + " BLOB" + ")";
 
     private static DatabaseHelper instance;
 
+    //Only allowing one instance of db
     public static synchronized DatabaseHelper getHelper(Context context) {
         if (instance == null) {
             instance = new DatabaseHelper(context);
@@ -59,45 +46,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         //Create the tables
         db.execSQL(CREATE_TABLE_PROFILE);
-        db.execSQL(CREATE_TABLE_LESSON);
-        db.execSQL(CREATE_TABLE_PRACTICE);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //Drop table if exist
         db.execSQL("DROP TABLE IF EXISTS " + PROFILE_TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + LESSON_TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + PRACTICE_TABLE_NAME);
         //Create the tables
         onCreate(db);
 
-    }
-
-    //add in a new profile
-    public void addProfile(ProfileModel profile) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        //put in values of id + name
-        values.put(PROFILE_ID,profile.getId());
-        values.put(PROFILE_NAME,profile.getName());
-        //insert values
-        db.insert(PROFILE_TABLE_NAME, null, values);
-        //close db connection
-        db.close();
-    }
-
-    //view the profiles
-    public ProfileModel viewProfile(int id) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(PROFILE_TABLE_NAME,new String[] {
-                PROFILE_ID,PROFILE_NAME}, PROFILE_ID + "=?",
-                new String[] {String.valueOf(id)},null,null,null,null);
-        if (cursor != null)
-            cursor.moveToFirst();
-            ProfileModel profile = new ProfileModel(Integer.parseInt(cursor.getString(0)),
-                    cursor.getString(1));
-        return profile;
     }
 
 }
--- app/src/main/java/team2/com/easyaspi/db/ProfileDaoImplementation.java
+++ app/src/main/java/team2/com/easyaspi/db/ProfileDaoImplementation.java
@@ -1,5 +1,6 @@
 package team2.com.easyaspi.db;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.List;
@@ -10,9 +11,16 @@ import java.util.List;
 
 public class ProfileDaoImplementation implements IProfileDao {
     public SQLiteDatabase db;
+    public static final String PROFILE_TABLE_NAME = "profile";
+    public static final String PROFILE_ID = "id";
+    public static final String PROFILE_NAME = "name";
+
     @Override
     public void add(ProfileModel profile) {
-       
+        ContentValues values = new ContentValues();
+        values.put(PROFILE_ID,profile.getId());
+        values.put(PROFILE_NAME,profile.getName());
+        db.insert(PROFILE_TABLE_NAME,null,values);
     }
 
     @Override
