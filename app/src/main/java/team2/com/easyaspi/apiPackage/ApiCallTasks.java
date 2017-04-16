87c7fb47fba045dea0c2a532d6ded94b85b79754
domlau <dominic.k.y.lau@gmail.com>
2017-04-16 17:16:02 -0400

    Fixed bugs

--- app/src/main/java/team2/com/easyaspi/apiPackage/ApiCallTasks.java
+++ app/src/main/java/team2/com/easyaspi/apiPackage/ApiCallTasks.java
@@ -14,16 +14,20 @@ import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by DLau on 2017-04-09.
+ * File name: ApiCallTasks.java
+ * File Description: Class that handles all API async tasks
  */
 
 public class ApiCallTasks {
 
+    //Create instance of the AsyncHttpClient
     private static AsyncHttpClient client = new AsyncHttpClient();
+    //Declare Variable for URL
     private static String BASE_URL_NOTIFICATION ="https://easyaspi-api.herokuapp.com/";
 
 
-    public static void getNotifications(Context context, String url, Header[] headers, RequestParams requestParams, JsonHttpResponseHandler responseHandler) {
-
+    //method getNotifications that will receive context, url, header, request parms, responsehandler
+        public static void getNotifications(Context context, String url, Header[] headers, RequestParams requestParams, JsonHttpResponseHandler responseHandler) {
         client.get(context,getAbsoluteUrl(url),headers,requestParams,responseHandler);
     }
     private static String getAbsoluteUrl(String relativeUrl) {
